90585d8554c2ec6f689880648433ae0a752be4e6
Julio Oliveira <julioccolv@gmail.com>
2022-03-17 22:13:11 -0300

    test: ensure MinLengthValidation returns error if value is invalid

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,10 @@
+import { InvalidFieldError } from "@/validation/errors/invalid-field-error"
+import { MinLengthValidation } from "./min-length-validation"
+
+describe('MinLengthValidation', () => {
+    test('should return error if value is invalid', () => {
+        const sut = new MinLengthValidation('field', 5)
+        const error = sut.validate('123')
+        expect(error).toEqual(new InvalidFieldError())
+    })
+})
