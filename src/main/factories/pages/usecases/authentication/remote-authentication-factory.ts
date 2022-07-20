746a81521b5c60ad8b811216a83b5410b41c7bfb
JulioOLV <julioccolv@gmail.com>
2022-07-20 08:29:42 -0300

    refactor: move facotries to their own files

---
+++ src/main/factories/pages/usecases/authentication/remote-authentication-factory.ts
@@ -0,0 +1,8 @@
+import { RemoteAuthentication } from "@/data/usecases/authentication/remote-authentication"
+import { Authentication } from "@/domain/usecases"
+import { makeApiUrl } from "@/main/factories/http/api-url-factory"
+import { makeAxiosHttpClient } from '../../../http/axios-http-client-factory'
+
+export const makeRemoteAuthentication = (): Authentication => {
+  return new RemoteAuthentication(makeApiUrl(), makeAxiosHttpClient())
+}
