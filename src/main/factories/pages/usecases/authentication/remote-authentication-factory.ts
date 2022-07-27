0b7f2c4a232815838e57aff72c13ffe760709fb3
JulioOLV <julioccolv@gmail.com>
2022-07-27 08:39:29 -0300

    refactor: change api url factory to receive path by param

--- src/main/factories/pages/usecases/authentication/remote-authentication-factory.ts
+++ src/main/factories/pages/usecases/authentication/remote-authentication-factory.ts
@@ -1,8 +1,8 @@
-import { RemoteAuthentication } from "@/data/usecases/authentication/remote-authentication"
-import { Authentication } from "@/domain/usecases"
-import { makeApiUrl } from "@/main/factories/http/api-url-factory"
+import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication'
+import { Authentication } from '@/domain/usecases'
+import { makeApiUrl } from '@/main/factories/http/api-url-factory'
 import { makeAxiosHttpClient } from '../../../http/axios-http-client-factory'
 
 export const makeRemoteAuthentication = (): Authentication => {
-  return new RemoteAuthentication(makeApiUrl(), makeAxiosHttpClient())
+  return new RemoteAuthentication(makeApiUrl('/login'), makeAxiosHttpClient())
 }
