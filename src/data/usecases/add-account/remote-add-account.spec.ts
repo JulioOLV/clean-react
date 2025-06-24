06aeb261075d5a73e3b2e5cc3e6af19cac8e5d5f
Julio Oliveira <juliooliveira@MacBook-Pro-de-Gabrielle.local>
2025-06-24 11:02:47 -0300

    test: add verification for HttpClient body in RemoteAddAccount

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -27,4 +27,11 @@ describe('RemoteAddAccount', () => {
     await sut.add(mockAddAccount())
     expect(httpPostClientSpy.url).toBe(url)
   })
+
+  test('Should call HttpClient with correct body', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const addAccountParams = mockAddAccount()
+    await sut.add(addAccountParams)
+    expect(httpPostClientSpy.body).toEqual(addAccountParams)
+  })
 })
