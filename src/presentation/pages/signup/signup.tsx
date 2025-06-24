4bf0813e5fa754e0db43535112a0f2e589ec335e
Julio Oliveira <juliooliveira@MacBook-Pro-de-Gabrielle.local>
2025-06-24 18:53:33 -0300

    feat: enhance SignUp component with state management and navigation link

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './signup-styles.scss'
 import { Footer, Input, LoginHeader, FormStatus } from '@/presentation/components'
 import Context from '@/presentation/context/form/form-context'
-import { Link } from 'react-router-dom'
 
 const SignUp: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    emailError: 'Campo obrigatório',
+    passwordConfirmationError: 'Campo obrigatório',
+    nameError: 'Campo obrigatório',
+    passwordError: 'Campo obrigatório',
+    mainError: ''
+  })
+
   return (
     <div className={Styles.signup}>
       <LoginHeader />
-      <Context.Provider value={{ state: {} }}>
+      <Context.Provider value={{ state }}>
         {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
         <form className={Styles.form}>
           <h2>Criar Conta</h2>
@@ -16,10 +24,10 @@ const SignUp: React.FC = () => {
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
           <Input type="password" name="password" placeholder="Digite sua senha" />
           <Input type="password" name="passwordConfirmation" placeholder="Confirme sua senha" />
-          <button className={Styles.submit} type="submit">Entrar</button>
-          <Link to="/login" className={Styles.link}>
+          <button data-testid="submit" disabled className={Styles.submit} type="submit">Entrar</button>
+          <span className={Styles.link}>
             Voltar para Login
-          </Link>
+          </span>
           <FormStatus />
         </form>
       </Context.Provider>
