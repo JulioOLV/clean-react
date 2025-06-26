26386083fc1618f9e78b1e46a7adda85afc8e63e
Julio Oliveira <juliooliveira@MacBook-Pro-de-Gabrielle.local>
2025-06-26 18:25:03 -0300

    test: ensure AddAccount is not called when form is invalid

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -162,4 +162,13 @@ describe('Login component', () => {
 
     expect(addAccountSpy.callsCount).toBe(1)
   })
+
+  test('Should not call AddAccount if form is invalid', async () => {
+    const validationError = faker.random.words()
+    const { sut, addAccountSpy } = makeSut({ validationError })
+
+    await simulateValidSubmit(sut)
+
+    expect(addAccountSpy.callsCount).toBe(0)
+  })
 })
