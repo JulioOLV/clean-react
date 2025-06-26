2ac5131cb98e4ce52ae839fba90c7a91f76c2c42
Julio Oliveira <juliooliveira@MacBook-Pro-de-Gabrielle.local>
2025-06-25 21:24:02 -0300

    test: add validation success state check for email field in SignUp component tests

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -78,4 +78,10 @@ describe('Login component', () => {
     Helper.populateField(sut, 'name')
     Helper.testStatusForField(sut, 'name')
   })
+
+    test('Should show valid email state if Validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'email')
+    Helper.testStatusForField(sut, 'email')
+  })
 })
