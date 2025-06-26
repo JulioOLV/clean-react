38ac19d78311d47f4b58546a43e49f7b261359a6
Julio Oliveira <juliooliveira@MacBook-Pro-de-Gabrielle.local>
2025-06-26 18:41:26 -0300

    test: add error handling for SaveAccessToken failure in Login and SignUp tests

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -174,7 +174,7 @@ describe('Login component', () => {
   test('Should present error if SaveAccessToken fails', async () => {
     const { sut, saveAccessTokenMock } = makeSut()
     const error = new InvalidCredentialsError()
-    jest.spyOn(saveAccessTokenMock, 'save').mockReturnValueOnce(Promise.reject(error))
+    jest.spyOn(saveAccessTokenMock, 'save').mockRejectedValueOnce(error)
 
     await simulateValidSubmit(sut)
 
