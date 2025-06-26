026090e70b2e588c5e1d0954ddca008c797d5923
Julio Oliveira <juliooliveira@MacBook-Pro-de-Gabrielle.local>
2025-06-25 21:15:39 -0300

    feat: update email state initialization and validation handling in SignUp component

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -13,7 +13,8 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
     isLoading: false,
     name: '',
     nameError: '',
-    emailError: 'Campo obrigatório',
+    email: '',
+    emailError: '',
     passwordConfirmationError: 'Campo obrigatório',
     passwordError: 'Campo obrigatório',
     mainError: ''
@@ -23,8 +24,9 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
     setState({
       ...state,
       nameError: validation?.validate('name', state.name),
+      emailError: validation?.validate('email', state.email),
     })
-  }, [state.name])
+  }, [state.name, state.email])
 
   return (
     <div className={Styles.signup}>
