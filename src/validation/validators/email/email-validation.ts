737eb0db1e8a60ecaad3adf0d1fe43698776283e
Julio Oliveira <juliooliveira@MacBook-Pro-de-Gabrielle.local>
2025-06-27 14:47:46 -0300

    refactor: update validation methods to accept objects instead of strings

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -4,7 +4,8 @@ import { FieldValidation } from '@/validation/protocols/field-validation'
 export class EmailValidation implements FieldValidation {
   constructor (readonly field: string) {}
 
-  validate (value: string): Error {
+  validate (input: object): Error {
+    const value = input[this.field]
     const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     return (!value || emailRegex.test(value)) ? null : new InvalidFieldError()
   }
