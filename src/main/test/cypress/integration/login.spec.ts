cd620206133f8bb0bea82470893af2a3d5d4fd3a
JulioOLV <julioccolv.dev@gmail.com>
2025-07-07 21:02:06 -0300

    test: refactor login form test to use beforeEach for setup

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,16 @@
 describe('Login page', () => {
-  it('Should display the login form', () => {
+  beforeEach(() => {
     cy.visit('login');
+  })
+
+  it('Should display the login form', () => {
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo obrigatorio')
+      .should('contain.text', '🔴')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo obrigatorio')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   });
 })
