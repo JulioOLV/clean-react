fdeed6e7b8d1b25b52d8e6a14dcf2e376d3c491d
JulioOLV <julioccolv.dev@gmail.com>
2025-07-07 21:31:15 -0300

    test: add error handling for invalid credentials in login form

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,7 @@
 import faker from 'faker'
 
+const baseUrl: string = Cypress.config().baseUrl
+
 describe('Login page', () => {
   beforeEach(() => {
     cy.visit('login');
@@ -50,5 +52,21 @@ describe('Login page', () => {
       .should('contain.text', '🟢')
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error if invalid credentials are provided', () => {
+    cy.getByTestId('email')
+      .focus()
+      .type(faker.internet.email())
+    cy.getByTestId('password')
+      .focus()
+      .type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').click()
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner').should('exist')
+      .getByTestId('main-error').should('not.exist')
+      .getByTestId('spinner').should('not.exist')
+      .getByTestId('main-error').should('exist')
+    cy.url().should('eq', `${baseUrl}/login`)
   })
 })
