3e5f13e4eb3cf85a1a534cdb0d6f67032ac20b14
JulioOLV <julioccolv.dev@gmail.com>
2025-07-07 22:16:58 -0300

    style: update input component styles for improved focus and label behavior

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import Styles from './input-styles.scss'
 import Context from '@/presentation/context/form/form-context'
 
@@ -6,6 +6,7 @@ type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
   const { state, setState } = useContext(Context)
+  const inputRef = useRef<HTMLInputElement>(null)
   const error = state[`${(props || {}).name}Error`]
 
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
@@ -29,8 +30,19 @@ const Input: React.FC<Props> = (props: Props) => {
 
   return (
     <div className={Styles['input-wrap']}>
-      <input {...props} data-testid={(props || {}).name} readOnly onFocus={enableInput} onChange={handleChange} />
-      <span data-testid={`${(props || {}).name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
+      <input
+        {...props}
+        placeholder=''
+        data-testid={(props || {}).name}
+        readOnly
+        onFocus={enableInput}
+        onChange={handleChange}
+        ref={inputRef}
+      />
+      <label onClick={() => inputRef.current.focus()}>{props.placeholder}</label>
+      <span
+        data-testid={`${(props || {}).name}-status`}
+        title={getTitle()} className={Styles.status}>{getStatus()}</span>
     </div>
   )
 }
